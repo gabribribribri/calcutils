d0fab8180d0aa30e5622dbfaa2090ae1b73761e9
gabribribribri <fuelanemo149@gmail.com>
2025-02-02 00:53:58 +0100

    downgraded facPr

--- afacPr.py
+++ afacPr.py
@@ -1,28 +1,31 @@
-from inlib import parse_input
-
-
-def is_prime(x):
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
+def is_prime(x) :
+    for i in range(2, int(x**0.5)+1):
+        if x%i == 0 :
             return False
     return True
 
-to_dec = int(parse_input("> ", {str.isdigit:"only digits."}))
+while True :
+    try:
+        to_dec = int(input("> "))
+    except :
+        print("non.")
+        continue
+    break
 
 if is_prime(to_dec):
     print("Prime !!")
     exit()
 
 fac_pr = {}
-for i in range(2, to_dec+1):
-    if is_prime(i):
+for i in range(2, to_dec+1) :
+    if is_prime(i) :
         expo_i = 0
         while True:
-            if to_dec % i == 0:
+            if to_dec%i == 0 :
                 print("{} | {}".format(int(to_dec), i))
                 to_dec /= i
-                expo_i += 1
-            else:
+                expo_i = 0
+            else :
                 if expo_i != 0:
                     fac_pr[i] = expo_i
                 break
@@ -30,7 +33,6 @@ for i in range(2, to_dec+1):
         break
 
 print("1 |\n")
-
 
 for (coef, expo) in fac_pr.items():
     print("{}^{}*".format(coef, expo))
